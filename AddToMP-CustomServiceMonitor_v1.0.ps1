58400dde164c241daa952c0c783e1a52cafcea26
Ryan Irujo <ryan.irujo@gmail.com>
2013-09-05 10:26:10 -0400

    Update AddToMP-CustomServiceMonitor_v1.0.ps1

--- AddToMP-CustomServiceMonitor_v1.0.ps1
+++ AddToMP-CustomServiceMonitor_v1.0.ps1
@@ -103,12 +103,12 @@ try {
 	
 	Foreach ($Item in $ServiceMonitorCheck) {
 		If ($Item.DisplayName -eq $ServiceDisplayName) {
-			Write-Host "[$($ServiceDisplayName)] Service already exists in [$($ManagementPackDisplayName)]. Script will not exit"
+			Write-Host "[$($ServiceDisplayName)] - Service Monitor already exists in [$($ManagementPackDisplayName)]. Script will not exit"
 			exit 2;
 		}
 	}
 
-	Write-Host "[$($ServiceDisplayName)] Service was not found in [$($ManagementPackDisplayName)]. Script will continue."
+	Write-Host "[$($ServiceDisplayName)] - Service Monitor was not found in [$($ManagementPackDisplayName)]. Script will continue."
 
 
 	# Retrieving the Custom Class in the Management Pack.
