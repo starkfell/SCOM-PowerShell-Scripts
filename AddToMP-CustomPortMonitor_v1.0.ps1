8af8b5b4fcb3d483382bc676d78679fbd165318d
Ryan Irujo <ryan.irujo@gmail.com>
2013-10-07 14:48:49 -0400

    Update AddToMP-CustomPortMonitor_v1.0.ps1

--- AddToMP-CustomPortMonitor_v1.0.ps1
+++ AddToMP-CustomPortMonitor_v1.0.ps1
@@ -1,4 +1,4 @@
-#  --- [AddToMP-CustomPortMonitor_v1.0] PowerShell Script  ---
+#  --- [AddToMP-CustomPortMonitor_v1.1] PowerShell Script  ---
 #
 # Author(s):        Ryan Irujo
 # Inception:        09.22.2013
@@ -14,7 +14,9 @@
 param($ManagementServer,$ManagementPackDisplayName,$ServiceName,$ServiceDisplayName,$CheckStartupType)
 
 $ManagementServer           = "SCOMMS223.scom.local"
-$ManagementPackDisplayName  = "Custom Service Monitors - Base OS"
+#$ManagementPackDisplayName  = "Custom Service Monitors - Base OS"
+#$ManagementPackDisplayName  = "Test Port Monitor - 80"
+$ManagementPackDisplayName  = "Test Port Monitor - Sandbox"
 $ServiceName                = "RemoteRegistry"
 $ServiceDisplayName         = "Remote Registry"
 $CheckStartupType           = "True"
@@ -101,16 +103,45 @@ try {
 	$CustomClasses = $MP.GetClasses()[0]
 
 
-	# Getting Synthetic Transaction Library Reference
-	$SyntheticLibrary = "Microsoft.SystemCenter.SyntheticTransactions.Library"
-	$MPToAdd          = $MG.GetManagementPacks($SyntheticLibrary)[0]
-	$MPAlias          = "MicrosoftSystemCenterSyntheticTransactionsLibrary"
+	# Checking to see if Synthetic Transactions Library Reference already exists in Management Pack.
+	$SyntheticLibrary                = "Microsoft.SystemCenter.SyntheticTransactions.Library"
+	$CheckIfSyntheticReferenceExists = $MP.get_References().ContainsKey("MicrosoftSystemCenterSyntheticTransactionsLibrary")
+	If ($CheckIfSyntheticReferenceExists -eq $true) {
+		Write-Host "Synthetic Transactions Library already exists in [$($ManagementPackDisplayName)], Script will continue..."
+		}
+	Else {
+		# Getting Synthetic Transaction Library Reference from Management Server.
+		$MPToAdd_1          = $MG.GetManagementPacks($SyntheticLibrary)[0]
+		$MPAlias_1          = "MicrosoftSystemCenterSyntheticTransactionsLibrary"
 	
+		# Adding Synthetic Transaction Library to Management Pack.
+		$MPReference_1      = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackReference($MPToAdd_1)
+		$MP.References.Add($MPAlias_1,$MPReference_1)
+		Write-Host "Synthetic Transactions Library successfully added to [$($ManagementPackDisplayName)]."
+		}
+
+
+	# Checking to see if Microsoft Windows Library Reference already exists in Management Pack.
+	$WindowsLibrary                = "Microsoft.Windows.Library"
+	$CheckIfWindowsReferenceExists = $MP.get_References().ContainsKey("Windows")
+	If ($CheckIfWindowsReferenceExists -eq $true) {
+		Write-Host "Microsoft Windows Library already exists in [$($ManagementPackDisplayName)], Script will continue..."
+		}
+	Else {
+		# Getting Microsoft Windows Library Reference from Management Server.
+		
+		$MPToAdd_2          = $MG.GetManagementPacks($WindowsLibrary)[0]
+		$MPAlias_2          = "Windows"
 	
-	# Adding Synthetic Transaction Library to Management Pack
-	$MPReference      = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackReference($MPToAdd)
-	$MP.References.Add($MPAlias,$MPReference)
-	
+		# Adding Microsoft Windows Library to Management Pack.
+		$MPReference_2      = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackReference($MPToAdd_2)
+		$MP.References.Add($MPAlias_2,$MPReference_2)
+		Write-Host "Microsoft Windows Library successfully added to [$($ManagementPackDisplayName)]."
+		}
+
+
+
+
 
 	# Creating TCP Port Custom Classes - TCPPortCheckPerspective
 	$TCPPortCheckCustomClass             = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackClass($MP,("TCPPortCheck_"+$Port.ToString().Replace("$","_")+"_"+[Guid]::NewGuid().ToString().Replace("-","")),"Public")
@@ -141,90 +172,92 @@ try {
 	$TCPPortRelationshipClassCriteria     = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackRelationshipCriteria("Name='System.Containment'")
 	$TCPPortRelationshipClassBase         = $MG.EntityTypes.GetRelationshipClasses($TCPPortRelationshipClassCriteria)[0]
 	$TCPPortRelationshipClass.Base        = $TCPPortRelationshipClassBase
-	$TCPPortRelationshipSourceEndpoint    = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackRelationshipEndpoint($TCPPortCheckGroupCustomClass,"TCPPortCheckGroupCustomClass_Source")
-	$TCPPortRelationshipTargetEndpoint    = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackRelationshipEndpoint($TCPPortCheckCustomClass,"TCPPortCheckCustomClass_Target")
-	$TCPPortRelationshipClass.Source      = $TCPPortRelationshipSourceEndpoint
-	$TCPPortRelationshipClass.Target      = $TCPPortRelationshipTargetEndpoint
+	$TCPPortRelationshipClass.DisplayName = "Group of Test Port Monitor - 81"
+	$TCPPortRelationshipSource            = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackRelationshipEndpoint($TCPPortRelationshipClass,"TCPPortCheckGroupCustomClass_Source")
+	$TCPPortRelationshipTarget            = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackRelationshipEndpoint($TCPPortRelationshipClass,"TCPPortCheckCustomClass_Target")	
+	$TCPPortRelationshipSource.Type       = $TCPPortCheckGroupCustomClass
+	$TCPPortRelationshipTarget.Type       = $TCPPortCheckCustomClass
+	$TCPPortRelationshipClass.Source      = $TCPPortRelationshipSource
+	$TCPPortRelationshipClass.Target      = $TCPPortRelationshipTarget
+
+	
+	
+	
+	# Creating a Module Composition Node Type with the ID of Probe.
+	$TCPPortCheckProbeModuleCompositionNodeType        = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleCompositionNodeType
+	$TCPPortCheckProbeModuleCompositionNodeType.ID     = "Probe"
+	
+	# Creating a Module Composition Node Type with the ID of Scheduler.
+	$TCPPortCheckSchedulerModuleCompositionNodeType    = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleCompositionNodeType
+	$TCPPortCheckSchedulerModuleCompositionNodeType.ID = "Scheduler"	
+	
 	
 	# Creating new Data Source Module Type
 	$TCPPortCheckDataSourceModule                    = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackDataSourceModuleType($MP,($TCPPortCheckCustomClass.ToString()+"_TCPPortCheckDataSource"),"Public")
+	$TCPPortCheckDataSourceModuleOutputTypeSourceMP  = $MG.GetManagementPacks($SyntheticLibrary)[0] 
+	$TCPPortCheckDataSourceModuleOutputType          = $TCPPortCheckDataSourceModuleOutputTypeSourceMP.GetDataType("Microsoft.SystemCenter.SyntheticTransactions.TCPPortCheckData")
+	$TCPPortCheckDataSourceModule.set_OutputType([Microsoft.EnterpriseManagement.Configuration.ManagementPackElementReference``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackDataType]]::op_implicit($TCPPortCheckDataSourceModuleOutputType))
 	
-	# Creating Data Source Collection for Data Source Module
+	$TCPPortCheckDataSourceModuleTypeReference                = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleTypeReference($TCPPortCheckDataSourceModule,"Scheduler")
+	$TCPPortCheckDataSourceModuleTypeReference.TypeID         = $MG.GetMonitoringModuleTypes("System.Scheduler")[0]
+	$TCPPortCheckDataSourceModuleTypeReferenceConfiguration   = "<Scheduler><SimpleReccuringSchedule><Interval Unit=`"Seconds`">120</Interval></SimpleReccuringSchedule><ExcludeDates /></Scheduler>"
+	$TCPPortCheckDataSourceModuleTypeReference.Configuration  = $TCPPortCheckDataSourceModuleTypeReferenceConfiguration
+	
+	$TCPPortCheckProbeActionModuleTypeReference               = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleTypeReference($TCPPortCheckDataSourceModule,"Probe")
+	$TCPPortCheckProbeActionModuleTypeReference.TypeID        = $MG.GetMonitoringModuleTypes("Microsoft.SystemCenter.SyntheticTransactions.TCPPortCheckProbe")[0]
+	$TCPPortCheckProbeActionModuleTypeReferenceConfiguration  = "<ServerName>SCOMDEVSRV</ServerName><Port>81</Port>"
+	$TCPPortCheckProbeActionModuleTypeReference.Configuration = $TCPPortCheckProbeActionModuleTypeReferenceConfiguration
+	
+	
+	$TCPPortCheckDataSourceModule.DataSourceCollection.Add($TCPPortCheckDataSourceModuleTypeReference)
+	$TCPPortCheckDataSourceModule.ProbeActionCollection.Add($TCPPortCheckProbeActionModuleTypeReference)
+	
+	$TCPPortCheckDataSourceModule.Node               = $TCPPortCheckProbeModuleCompositionNodeType
+	$TCPPortCheckDataSourceModule.Node.NodeCollection.Add($TCPPortCheckSchedulerModuleCompositionNodeType)
+	
+	
+	
+	#$TCPPortCheckDataSourceModule.Node               = $TCPPortCheckProbeModuleCompositionNodeType
+	#$TCPPortCheckDataSourceModule.Node.NodeCollection.Add($TCPPortCheckSchedulerModuleCompositionNodeType)
+	
+	<## Creating Data Source Collection for Data Source Module
 	$TCPPortCheckDataSourceCollection                = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackDataSourceModule($TCPPortCheckDataSourceModule,"Scheduler")
 	$TCPPortCheckDataSourceCollection.TypeID         = $MG.GetMonitoringModuleTypes("System.Scheduler")[0]
 	$TCPPortCheckDataSourceCollectionConfiguration   = "<Scheduler><SimpleReccuringSchedule><Interval Unit=`"Seconds`">120</Interval></SimpleReccuringSchedule><ExcludeDates /></Scheduler>"
 	$TCPPortCheckDataSourceCollection.Configuration  = $TCPPortCheckDataSourceCollectionConfiguration
-
+	
+	
 	# Creating Probe Action Collection for Data Source Module
     $TCPPortCheckProbeActionCollection               = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackProbeActionModule($TCPPortCheckDataSourceModule,"Probe")
 	$TCPPortCheckProbeActionCollection.TypeID        = $MG.GetMonitoringModuleTypes("Microsoft.SystemCenter.SyntheticTransactions.TCPPortCheckProbe")[0]
-	$TCPPortCheckProbeActionCollectionConfiguration  = "<ServerName>SCOMDEVSRV</ServerName><Port>80</Port>"
+	$TCPPortCheckProbeActionCollectionConfiguration  = "<ServerName>SCOMDEVSRV</ServerName><Port>81</Port>"
 	$TCPPortCheckProbeActionCollection.Configuration = $TCPPortCheckProbeActionCollectionConfiguration
+	
 
+	$TCPPortCheckDataSourceModule.Node               = $TCPPortCheckProbeModuleCompositionNodeType
+	$TCPPortCheckDataSourceModule.Node.NodeCollection.Add($TCPPortCheckSchedulerModuleCompositionNodeType)
 
-	#$TCPPortCheckDataSourceModule.set_OutputType([Microsoft.EnterpriseManagement.Configuration.ManagementPackElementReference``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackProbeActionModule]]::op_implicit($TCPPortCheckProbeActionCollection))
+	#>
 
+	#$TCPPortCheckDataSourceModule.ProbeActionCollection.Add([Microsoft.EnterpriseManagement.Configuration.ManagementPackSubElementCollection``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleTypeReference]]$TCPPortCheckProbeActionCollection)
+	#$TCPPortCheckDataSourceModule.ProbeActionCollection.Add([Microsoft.EnterpriseManagement.Configuration.ManagementPackSubElementCollection``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleTypeReference]]$TCPPortCheckProbeActionTypeReference)
+	#$TCPPortCheckProbeActionTypeReference = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackModuleTypeReference($TCPPortCheckDataSourceModule,"ProbeRef")
+	#$TCPPortCheckDataSourceModule.set_OutputType([Microsoft.EnterpriseManagement.Configuration.ManagementPackElementReference``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackDataType]]::op_implicit($TCPPortCheckDataSourceModuleOutputType))
+	#$TCPPortCheckDataSourceModule.CreateNavigator()
+	#$TCPPortCheckDataSourceModuleOutputTypeSourceMP  = $MG.GetManagementPacks($SyntheticLibrary)[0]  
+	#$TCPPortCheckDataSourceModuleOutputType          = $TCPPortCheckDataSourceModuleOutputTypeSourceMP.GetDataType("Microsoft.SystemCenter.SyntheticTransactions.TCPPortCheckData")
+	#$TCPPortCheckDataSourceModule.set_OutputType([Microsoft.EnterpriseManagement.Configuration.ManagementPackElementReference``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackDataType]]::op_implicit($TCPPortCheckDataSourceModuleOutputType))
+	#$TCPPortCheckDataSourceModuleImplementation      = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackDataType(
+	#$TCPPortCheckDataSourceModuleOutputType          = $MG.GetMonitoringModuleTypes("Microsoft.SystemCenter.SyntheticTransactions.TCPPortCheckProbe")[0]
+	
+
+	<#
 	# Creating New Port Monitor
 	$PortMonitorTypeQuery = "Name = 'Microsoft.SystemCenter.SyntheticTransactions.TCPPortCheckProbe'"
 	$PortMonitorCriteria  = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackUnitMonitorTypeCriteria($PortMonitorTypeQuery)
 	$PortMonitorType      = $MG.GetUnitMonitorTypes($PortMonitorCriteria)[0]
+	#>
 	
-	
-
-	# Creating New Service Monitor
-	$MonitorTypeQuery     = "Name = 'Microsoft.Windows.CheckNTServiceStateMonitorType'"
-	$MonitorCriteria      = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackUnitMonitorTypeCriteria($MonitorTypeQuery)
-	$ServiceMonitorType   = $MG.GetUnitMonitorTypes($MonitorCriteria)[0]
-	$Monitor              = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackUnitMonitor($MP,($ServiceName.ToString().Replace("$","_")+"_"+[Guid]::NewGuid().ToString().Replace("-","")),"Public")
-
-
-	# Setting new New Monitor Up as a Service Monitor and targeting the Hosts of the Group in the MP.
-	$Monitor.set_DisplayName($ServiceDisplayName)
-	$Monitor.set_Category("Custom")
-	$Monitor.set_TypeID($ServiceMonitorType)
-	$Monitor.set_Target($CustomClasses)
-
-
-	# Configure Monitor Alert Settings
-	$Monitor.AlertSettings = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackMonitorAlertSettings
-	$Monitor.AlertSettings.set_AlertOnState("Error")
-	$Monitor.AlertSettings.set_AutoResolve($true)
-	$Monitor.AlertSettings.set_AlertPriority("Normal")
-	$Monitor.AlertSettings.set_AlertSeverity("Error")
-	$Monitor.AlertSettings.set_AlertParameter1("`$Target/Host/Property[Type=`"Windows!Microsoft.Windows.Computer`"]/NetworkName$")
-	$Monitor.AlertSettings.AlertMessage
-
-
-	# Configure Alert Settings - Alert Message
-	$AlertMessage = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackStringResource($MP, ("CustomServiceMonitorAlertMessage_"+$ServiceName.ToString().Replace("$","_")+"_"+[Guid]::NewGuid().ToString().Replace("-","")))
-	$AlertMessage.set_DisplayName("The $($ServiceDisplayName) Service has Stopped")
-	$AlertMessage.set_Description("The $($ServiceDisplayName) Service has Stopped on {0}")
-	$Monitor.AlertSettings.set_AlertMessage($AlertMessage)
-
-
-	# Configure Health States for the Monitor
-	$HealthyState = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackUnitMonitorOperationalState($Monitor, "Success")
-	$ErrorState   = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackUnitMonitorOperationalState($Monitor, "Error")
-	$HealthyState.set_HealthState("Success")
-	$HealthyState.set_MonitorTypeStateID("Running")
-	$ErrorState.set_HealthState("Error")
-	$ErrorState.set_MonitorTypeStateID("NotRunning")
-	$Monitor.OperationalStateCollection.Add($HealthyState)
-	$Monitor.OperationalStateCollection.Add($ErrorState)
-
-
-	# Specifying Service Monitoring Configuration
-	$MonitorConfig = "<ComputerName>`$Target/Host/Property[Type=`"Windows!Microsoft.Windows.Computer`"]/NetworkName$</ComputerName>
-	                  <ServiceName>$($ServiceName)</ServiceName>
-					  <CheckStartupType>$($CheckStartupType)</CheckStartupType>"
-
-	$Monitor.set_Configuration($MonitorConfig)
-
-
-	# Specify Parent Monitor by ID
-	$MonitorCriteria  = New-Object Microsoft.EnterpriseManagement.Configuration.ManagementPackMonitorCriteria("Name='System.Health.AvailabilityState'")
-	$ParentMonitor    = $MG.GetMonitors($MonitorCriteria)[0]
-	$Monitor.ParentMonitorID = [Microsoft.EnterpriseManagement.Configuration.ManagementPackElementReference``1[Microsoft.EnterpriseManagement.Configuration.ManagementPackAggregateMonitor]]::op_implicit($ParentMonitor)
 
 
 	# Applying changes to the Management Pack in the SCOM Database.	
